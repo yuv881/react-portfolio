a71313e141d9e9547e565745e450b40841fb3548
unknown <yshaktawat116@gmail.com>
2025-12-26 14:54:43 +0530

    add pagespeed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div className="app">
+      <SpeedInsights />
       <Navbar />
       <Hero />
       <About />
